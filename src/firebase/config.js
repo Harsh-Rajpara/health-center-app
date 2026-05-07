097ba8bfe5c79613a3ee57bf79e358a22a363625
Harsh Rajpara <hrajpara787@gmail.com>
2026-05-07 15:44:13 +0530

    update env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -22,12 +22,12 @@ import { getStorage } from "firebase/storage";
 
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -37,7 +37,7 @@ export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
 
 // Admin email
-const ADMIN_EMAIL = REACT_APP_ADMIN_EMAIL;
+const ADMIN_EMAIL = process.env.REACT_APP_ADMIN_EMAIL;
 
 // Sign in with Google
 export const signInWithGoogle = async () => {
